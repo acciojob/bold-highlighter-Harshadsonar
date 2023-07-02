5f2323eb350e2c7a37bd05222609d711fbc0e1ce
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 14:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let bold_Items;
+window.onload = getBold_Items();
+
+function getBold_Items() {
+	bold_Items = document.getElementByTagName('strong');
+}
 function highlight() {
     for(let i=0; i<bold_Items.length; i++){
 		bold_items[i].style.color = "green";
