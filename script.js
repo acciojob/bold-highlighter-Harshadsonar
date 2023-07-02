75af11239baf37efbb7044c0a7f5cbe444b62fbf
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-02 14:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-
 function highlight() {
-	let boldElements = document.getElementByTagName("strong");
+	let boldElements = document.getElementsByTagName("strong");
 	
     for(let i=0; i<boldElements.length; i++){
 		boldElements[i].style.color = "green";
 	}
-
-
 }
 
-
 function return_normal() {
-	let boldElements = document.getElementByTagName("strong");
+	let boldElements = document.getElementsByTagName("strong");
     for(let i=0; i<boldElements.length; i++){
 		boldElements[i].style.color = "black";
 	}
-
-    
 }
+
